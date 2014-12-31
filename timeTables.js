587bfde8adf0d697e9a84acd2ebfcf3e27fbbc9c
Anton Krug <antonkrug@gmail.com>
2014-12-31 01:01:24 +0000

    second semester and less glitchy behaviour

--- timeTables.js
+++ timeTables.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name WIT timetable automatic selection
 // @namespace http://antonkrug.eu
-// @version 0.3
+// @version 0.4
 // @description It selects your desired course
 // @match http://studentssp.wit.ie/Timetables/StudentGroupTT.aspx
 // @copyright 2014+ Anton Krug
@@ -9,15 +9,28 @@
 // ==/UserScript==
 
 $(document).ready(function() {
-if ($("#CboPOS").val()="") {
-	$("#CboPOS").val('85EA4CF769354ECAD9F18363EC561527');
-	$("#CboStudParentGrp").val('kcomp_b2-X');
-	$("#BtnRetrieve").click();
-} else {
-	$('#divTT table').first().remove();
-	$('#divTT *').each(function() {
-		xsize= parseInt($(this).css('font-size')) + 1;    
-		$(this).css('font-size', '9px');
-	});
+	var school='SS';									//School of science
+    var department='548715F70874B2B1561DDC98FE61E5C0';	//Computing, maths & physics department
+    var course='85EA4CF769354ECAD9F18363EC561527';		//Applied Computing Y2
+    var group='kcomp_b2-X';								//Students group
+    var week='20';										//Week 20
+
+    if ($("#CboPOS").val()=="%" && $("#CboPOS").val()=="%" ) {
+		$("#CboSchool").val(school);
+		$("#CboDep").val(department);
+		$("#CboPOS").val(course);
+		$("#CboStudParentGrp").val(group);
+		$("#CboWeeks").val(week);
+		$("#BtnRetrieve").click();
+	} else {
+		//Re-select some missing selections just for visuals.
+		//And if somebody wants select something else (different week) so he will not be confused
+		$("#CboStudParentGrp").val(group);
+		//alter presentation of the time tables
+		$('#divTT table').first().remove();			//remove logo
+		$('#divTT *').each(function() {				//alter fonts
+			xsize= parseInt($(this).css('font-size')) + 1;    
+			$(this).css('font-size', '9px');
+		});
 	}
 });
